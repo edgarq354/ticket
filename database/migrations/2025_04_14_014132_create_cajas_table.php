ff81cb54a6f1a4e444b3e80054b0ed8504d6b855
Edgar Elio <lebam.scalderon@gmail.com>
2025-04-13 23:42:19 -0400

    Configurando bd

---
+++ database/migrations/2025_04_14_014132_create_cajas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cajas', function (Blueprint $table) {
+            $table->id();
+            $table->string('caja',10);
+            $table->string('estado',10)->default('Activo');
+            $table->foreignId('user_id')->constrained('users')
+            ->onUpdate('cascade')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cajas');
+    }
+};
